7c018a46e2aef0a6db2874d3e27e731f8fbcbb35
tolojo <73295309+tolojo@users.noreply.github.com>
2020-11-22 21:03:16 +0000

    Final, still with problems

--- src/main/java/tutorial1/tutorial1/models/Enrolment.java
+++ src/main/java/tutorial1/tutorial1/models/Enrolment.java
@@ -6,8 +6,32 @@ public class Enrolment{
     private double grade;
     public Enrolment (Student student,Unit unit, double grade){
         this.student = student;
-       this.unit = unit;
+        this.unit = unit;
         this.grade=grade;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
     }
 
 }
--- src/main/java/tutorial1/tutorial1/models/UnitRepository.java
+++ src/main/java/tutorial1/tutorial1/models/UnitRepository.java
@@ -1,5 +1,32 @@
 package tutorial1.tutorial1.models;
 
+
+import java.util.ArrayList;
+
+
+
 public class UnitRepository {
     
+    private static ArrayList<UnitRepository> unit = new ArrayList<UnitRepository>();
+    public static void populate() {
+    Unit u; // auxiliary variable
+    u = new Unit(15,"AB",10);
+    unit.add(u);
+    }
+   
+    public static ArrayList<UnitRepository> getUnits() { return unit; }
+    
+    public static Unit getUnit(int id) {
+        for (Unit units: unit)
+            if (unit.getId() == id)
+                return units;
+        return null;
+        }
+        
+
+
+    
+
+
+
 }
